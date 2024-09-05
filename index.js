ee26fe59c12baba5eb3a8eaeeb82c2758eadb1f8
Nik-F-98 <irkafkin@gmail.com>
2024-09-05 21:29:10 +0800

    wip

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ import database from "./database/connection.js";
 const app = express();
 const PORT = 3000;
 
+var corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 // middleware
-app.use(cors()); // Enable CORS for all routes and origins
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions)); // Enable CORS for all routes and origins
 
 // routes
 app.use(router);
